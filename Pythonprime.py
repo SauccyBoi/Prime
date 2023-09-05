26b1699ab66c13f992d28769ae343e6d8fba76dc
SauccyBoi <131536776+SauccyBoi@users.noreply.github.com>
2023-09-05 13:06:50 +0530

    Create Pythonprime.py
    yes

---
+++ Pythonprime.py
@@ -0,0 +1,21 @@
+def prime():
+    try:
+        if_prime = int(input("Enter a number"))
+        divisor = 2
+        Prime_Condition = True
+        while divisor < if_prime:
+            if if_prime % divisor == 0:
+                Prime_Condition = False
+                break
+            elif if_prime % divisor != 0:
+                if divisor == if_prime - 1:
+                    Prime_Condition = True
+            divisor = divisor + 1
+        if Prime_Condition == True:
+            print(if_prime,"is a prime number")
+        else:
+            print(if_prime,"is a composite number")
+        return(Prime_Condition)
+    except:
+        if_prime = int(input("Invalid input please try again"))
+print(prime())
